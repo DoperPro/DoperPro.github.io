822307c3082d76c6f37fbf39188d475a6c5c7eb9
red-wisp <59499834+red-wisp@users.noreply.github.com>
2020-05-04 01:04:03 +0400

    скрипт для 1 страницы

--- script_for_first.js
+++ script_for_first.js
@@ -1,2 +1,13 @@
+function ref(){
 let all_input = document.querySelectorAll("input")
-console.log(all_input);
+
+let obj = {};
+for(let i = 0; i < all_input.length;i++){
+obj[all_input[i].id] = all_input[i].value;
+}
+obj['id_doc'] = document.getElementById('id_doc').value;
+obj['taxpayer'] = document.getElementById('taxpayer').value;
+
+console.log(obj);
+}
+document.documentElement.addEventListener('click', ref);
