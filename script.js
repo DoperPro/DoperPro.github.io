808c02af55c8582980a823cbf0a0e0d7102bf12e
DoperPro <53624915+DoperPro@users.noreply.github.com>
2020-05-03 22:27:00 +0400

    scroll

--- script.js
+++ script.js
@@ -167,4 +167,21 @@ document.documentElement.addEventListener('keyup', fill_preview_table); //три
 
     };
 
+    $(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#button1').fadeIn();
+    } else {
+        $('#button1').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#button1").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+
 
