96e57b4d5567af4464db395fadfe40a14fb4f58d
red-wisp <59499834+red-wisp@users.noreply.github.com>
2020-05-05 17:57:05 +0400

    pdf filler

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const PDFF = require('pdffiller');          //работа с PDF
 const path = require('path');               //объект пути
 
-const ref_pdf =  path.resolve(__dirname,'3ndfl_2019.pdf');
-console.log(ref_pdf);
-const rez_pdf = path.resolve(__dirname,'rez.pdf');
+// const ref_pdf =  path.resolve(__dirname,'3ndfl_2019.pdf');
+// console.log(ref_pdf);
+// const rez_pdf = path.resolve(__dirname,'rez.pdf');
+const ref_pdf = "tt/in.pdf"
+const rez_pdf = "tt/out.pdf"
 
 const data = {
-    "Text6.0": "012345678901"
+    "text1": "012345678901"
 }
 PDFF.fillForm( ref_pdf, rez_pdf, data, function(err) {
     if (err) throw err;
