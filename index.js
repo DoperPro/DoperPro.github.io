2ffc0ef3b29cf2b83a28016f50d2c0441b9236f1
red-wisp <59499834+red-wisp@users.noreply.github.com>
2020-05-06 20:37:39 +0400

    перепиленно

--- index.js
+++ index.js
@@ -1,36 +1,8 @@
-const pdff = require('./pdfform');
-const fs = require('fs');
-const in_pdf_p = './tt/in.pdf';
-let out_buf;
-
-function toArrayBuffer(buf) {
-    var ab = new ArrayBuffer(buf.length);
-    var view = new Uint8Array(ab);
-    for (var i = 0; i < buf.length; ++i) {
-        view[i] = buf[i];
+var fill_pdf = require('fill-pdf-utf8');
+ 
+fill_pdf.generatePdf({fields:{name:'ЖОРА',inn:"0123456"}},'./tt/in.pdf',{fontSize: 16.0},'./tt/out.pdf',function (error, stdout, stderr) {
+    if(error){
+        throw error;
     }
-    return ab;
-}
-
-fs.readFileSync(in_pdf_p, (err, data) => {
-    if (err) throw err;
-    console.log(data);
-    let fil = {
-        name: 'FFFFF',
-    }
-    out_buf = pdff().transform(toArrayBuffer(data),fil);
-})
-
-function toBuffer(ab) {
-    var buf = Buffer.alloc(ab.byteLength);
-    var view = new Uint8Array(ab);
-    for (var i = 0; i < buf.length; ++i) {
-        buf[i] = view[i];
-    }
-    return buf;
-}
-fs.writeFileSync('./tt/out.pdf',toBuffer(out_buf),err=>{
-    if (err) throw err;
-    console.log('запись');
-    
+    console.log(stdout);
 })
